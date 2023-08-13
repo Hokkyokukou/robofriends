e6f610bcf843d9103f2df163ffc005ce11ee9f3c
Hokkyokukou <hokkyokukou27@gmail.com>
2023-08-13 19:23:12 +0200

    Add State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robofriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}></CardList>
-        </div>
-    );
-};
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        };
+    }
+    render() {
+        return (
+            <div className="tc">
+                <h1>Robofriends</h1>
+                <SearchBox />
+                <CardList robots={this.state.robots}></CardList>
+            </div>
+        );
+    }
+}
 
 export default App;
