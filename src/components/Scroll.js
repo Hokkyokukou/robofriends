6854f218247d08deb696b49bdc96ba44664661d4
Hokkyokukou <hokkyokukou27@gmail.com>
2024-03-22 19:40:04 +0100

    update

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,10 +4,16 @@ const Scroll = (props) => {
     //console.log(props)
     //return props.children
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+        <div
+            style={{
+                overflowY: 'scroll',
+                border: '5px solid black',
+                height: '800px',
+            }}
+        >
             {props.children}
         </div>
-    )
+    );
 };
 
 export default Scroll;
